0efe6386c425e3a63eb11b53accbc9f5045ebd04
cubiclesoft <webmaster@cubiclesoft.com>
2018-06-28 08:02:58 -0700

    Updated.

--- admin/install.php
+++ admin/install.php
@@ -33,6 +33,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title><?=htmlspecialchars($title)?> | Barebones CMS Admin Installer</title>
 <link rel="stylesheet" href="support/install.css" type="text/css" media="all" />
 <?php
